5594b7362f42692e171a95ba358e3089c02d940c
NikolajX4000 <supernikolaj@hotmail.com>
2019-02-07 08:40:44 +0100

    started TPS

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject gunpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBullet();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void ShootBullet()
+    {
+        GameObject obj = Instantiate(bullet, gunpoint.transform.position, gunpoint.transform.rotation);
+        obj.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, 10));
+    }
+}
