cf332f6977d19c22a0d27c5c4bec14357cfe8938
Nikolaj <supernikolaj@hotmail.com>
2019-02-07 12:12:46 +0100

    TPS done with object pooling

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,17 +6,26 @@ public class BulletScript : MonoBehaviour
 {
     public GameObject bullet;
     public GameObject gunpoint;
+    public int speed;
+    public int bulletPool = 20;
+    List<GameObject> bullets;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullets = new List<GameObject>();
+        for(int i = 0; i < bulletPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(bullet);
+            obj.SetActive(false);
+            bullets.Add(obj);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             ShootBullet();
         }
@@ -29,7 +38,18 @@ public class BulletScript : MonoBehaviour
 
     void ShootBullet()
     {
-        GameObject obj = Instantiate(bullet, gunpoint.transform.position, gunpoint.transform.rotation);
-        obj.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, 10));
+        //GameObject obj = Instantiate(bullet, gunpoint.transform.position, gunpoint.transform.rotation);
+        //obj.GetComponent<Rigidbody>().velocity = speed * gunpoint.transform.forward;
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            if (!bullets[i].activeInHierarchy)
+            {
+                bullets[i].transform.position = gunpoint.transform.position;
+                bullets[i].transform.rotation = gunpoint.transform.rotation;
+                bullets[i].SetActive(true);
+                bullets[i].GetComponent<Rigidbody>().velocity = speed * gunpoint.transform.forward;
+                break;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject enemy;
+    public int count;
+    public void SpawnEnemies()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(enemy, new Vector3(i* Random.Range(-10, 10), 0, i* Random.Range(-10, 10)), Quaternion.identity);
+        }
+    }
+}
